35428d10fba707c39539143faae54c8393243660
Nandini Jain <nandinijain2410@gmail.com>
2020-10-19 12:27:20 +0530

    Added Project

---
+++ build/precache-manifest.9a28946b3d619d98c38c72d0c58e000b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84dd86d1534e44e6d37cbd8fdf6e606c",
+    "url": "/Credit-Card-Checkout-Form/index.html"
+  },
+  {
+    "revision": "ed69d042d1f45283a2b9",
+    "url": "/Credit-Card-Checkout-Form/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "88b7282dc9a6587ffb86",
+    "url": "/Credit-Card-Checkout-Form/static/js/2.caafcda2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Credit-Card-Checkout-Form/static/js/2.caafcda2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed69d042d1f45283a2b9",
+    "url": "/Credit-Card-Checkout-Form/static/js/main.9fcbbb75.chunk.js"
+  },
+  {
+    "revision": "76cdf8bd91b22e1bcaf0",
+    "url": "/Credit-Card-Checkout-Form/static/js/runtime-main.a85e30a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Credit-Card-Checkout-Form/static/media/logo.5d5d9eef.svg"
+  }
+]);
